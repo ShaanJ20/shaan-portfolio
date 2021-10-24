638c896c4acfdeb70c841344b813938b162eef55
ShaanJ20 <shaan.johari18@gmail.com>
2021-10-24 17:01:48 +0530

    designed about card

--- src/components/homepage/Aboutcard.js
+++ src/components/homepage/Aboutcard.js
@@ -1,10 +1,16 @@
 import React from 'react'
+import aboutimg from './Assets/aboutimg.svg'
 import './CSS/aboutcard.css'
 
 export default function Aboutcard() {
     return (
-        <div>
-            
+        <div className="card" style={{width: '20.813rem'}}>
+            <img src={aboutimg} className="card-img-top" alt="aboutimg" />
+            <div className="card-body">
+                <h5 className="card-title">About<a style={{color:'#031B9A'}}>.</a></h5>
+                <p className="card-text">Know more about me, my skills, my hobbies, my background and other small things here!</p>
+                <a href="/" className="btn btn-primary">Explore.  🚀 </a>
+            </div>
         </div>
     )
 }
