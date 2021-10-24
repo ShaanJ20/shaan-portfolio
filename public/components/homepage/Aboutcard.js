c93b0809fd518ad74880d630033d1f0328ef6b5b
ShaanJ20 <shaan.johari18@gmail.com>
2021-10-24 16:10:32 +0530

    Setting up the components

---
+++ public/components/homepage/Aboutcard.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './CSS/aboutcard.css'
+
+export default function Aboutcard() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
---
+++ public/components/homepage/Projectscard.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './CSS/projectcards.css'
+
+export default function Projectscard() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
